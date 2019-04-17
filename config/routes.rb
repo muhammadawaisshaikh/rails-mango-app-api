9f2fd30cf533301e5577ae72773553a3e84f8e11
Muhammad Awais <ashaikh@creativechaos.co>
2019-04-17 18:58:36 +0500

    routes added in routes rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   api_version(:module => "V1", :path => {:value => "v1"}) do
+    resources :categories
+    resources :ratelists
+    resources :statuses
+    resources :stocks
   end  
+
   constraints subdomain: 'api' do
   end
+
 end

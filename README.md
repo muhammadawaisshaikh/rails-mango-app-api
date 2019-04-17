25ea33dcb5cc37486ec189065bc2d4c3dc451467
Muhammad Awais <ashaikh@creativechaos.co>
2019-04-17 18:49:47 +0500

    started api only app with postgres, setting subdomain to api in routes, init the versionist gem and adding api version in routes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rails-mango-app-api
 mango ordering app api only with rails5
+
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  api_version(:module => "V1", :path => {:value => "v1"}) do
+  end  
+  constraints subdomain: 'api' do
+  end
+end
